14ece5de413465dbe0b3af9896b1d49b1dddddea
CristelRR <cristel23rr@gmail.com>
2024-02-26 11:18:08 -0600

    2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SolicitudHorario from './components/solicitudHorario';
+import Prueba from './components/prueba';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<SolicitudHorario/>}></Route>
+          <Route path='/prueba' element={<Prueba/>}></Route>
+        </Routes> 
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/prueba.js
@@ -0,0 +1,69 @@
+const Prueba = () => {
+    return ( 
+        <div className="container">
+            
+        <div style={{width: 1440, height: 1024, position: 'relative', background: '#E9E9E9'}}>
+  <div style={{width: 1440, height: 82, left: 0, top: 0, position: 'absolute', background: '#5A9AFA'}} />
+  <div style={{width: 1228, height: 770, left: 106, top: 167, position: 'absolute', background: '#D9D9D9'}} />
+  <div style={{width: 1227, height: 94, left: 107, top: 170, position: 'absolute', background: 'rgba(89.67, 153.67, 249.67, 0.99)'}} />
+  <div style={{width: 767, height: 0, left: 720, top: 170, position: 'absolute', transform: 'rotate(90deg)', transformOrigin: '0 0', border: '3px #FFFCFC solid'}}></div>
+  <div style={{width: 1228, height: 0, left: 106, top: 264, position: 'absolute', border: '3px white solid'}}></div>
+  <div style={{width: 116, height: 56, left: 355, top: 189, position: 'absolute', color: 'white', fontSize: 40, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>turno</div>
+  <div style={{width: 376, height: 75, left: 827, top: 179, position: 'absolute', color: 'white', fontSize: 40, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Cantidad  empleados</div>
+  <div style={{width: 525, height: 84, left: 151, top: 326, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', border: '1px white solid'}} />
+  <div style={{width: 115, height: 84, left: 957, top: 326, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', border: '1px white solid'}} />
+  <div style={{width: 115, height: 84, left: 1145, top: 326, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', border: '1px white solid'}} />
+  <div style={{left: 1195, top: 344, position: 'absolute', color: 'black', fontSize: 40, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>-</div>
+  <div style={{width: 115, height: 84, left: 770, top: 326, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', border: '1px white solid'}} />
+  <div style={{width: 525, height: 84, left: 151, top: 440, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', border: '1px white solid'}} />
+  <div style={{width: 115, height: 84, left: 957, top: 440, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', border: '1px white solid'}} />
+  <div style={{left: 1003, top: 344, position: 'absolute', color: '#CBCBCB', fontSize: 40, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>0</div>
+  <div style={{width: 115, height: 84, left: 1145, top: 440, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', border: '1px white solid'}} />
+  <div style={{width: 115, height: 84, left: 770, top: 440, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', border: '1px white solid'}} />
+  <div style={{width: 525, height: 84, left: 151, top: 554, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', border: '1px white solid'}} />
+  <div style={{width: 115, height: 84, left: 957, top: 554, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', border: '1px white solid'}} />
+  <div style={{width: 115, height: 84, left: 1145, top: 554, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', border: '1px white solid'}} />
+  <div style={{left: 1003, top: 458, position: 'absolute', color: '#CBCBCB', fontSize: 40, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>0</div>
+  <div style={{width: 115, height: 84, left: 770, top: 554, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', border: '1px white solid'}} />
+  <div style={{width: 525, height: 84, left: 151, top: 668, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', border: '1px white solid'}} />
+  <div style={{width: 115, height: 84, left: 957, top: 668, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', border: '1px white solid'}} />
+  <div style={{width: 115, height: 84, left: 1145, top: 668, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', border: '1px white solid'}} />
+  <div style={{width: 115, height: 84, left: 770, top: 668, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', border: '1px white solid'}} />
+  <div style={{width: 525, height: 84, left: 151, top: 782, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', border: '1px white solid'}} />
+  <div style={{width: 115, height: 84, left: 957, top: 782, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', border: '1px white solid'}} />
+  <div style={{width: 115, height: 84, left: 1145, top: 782, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', border: '1px white solid'}} />
+  <div style={{width: 115, height: 84, left: 770, top: 782, position: 'absolute', background: 'white', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', border: '1px white solid'}} />
+  <div style={{left: 1003, top: 686, position: 'absolute', color: '#CBCBCB', fontSize: 40, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>0</div>
+  <div style={{left: 1003, top: 800, position: 'absolute', color: '#CBCBCB', fontSize: 40, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>0</div>
+  <div style={{left: 815, top: 344, position: 'absolute', color: 'black', fontSize: 40, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>+</div>
+  <div style={{left: 815, top: 458, position: 'absolute', color: 'black', fontSize: 40, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>+</div>
+  <div style={{left: 815, top: 572, position: 'absolute', color: 'black', fontSize: 40, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>+</div>
+  <div style={{left: 815, top: 686, position: 'absolute', color: 'black', fontSize: 40, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>+</div>
+  <div style={{left: 815, top: 800, position: 'absolute', color: 'black', fontSize: 40, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>+</div>
+  <div style={{left: 1195, top: 572, position: 'absolute', color: 'black', fontSize: 40, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>-</div>
+  <div style={{left: 1195, top: 686, position: 'absolute', color: 'black', fontSize: 40, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>-</div>
+  <div style={{left: 1195, top: 800, position: 'absolute', color: 'black', fontSize: 40, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>-</div>
+  <div style={{left: 1195, top: 458, position: 'absolute', color: 'black', fontSize: 40, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>-</div>
+  <div style={{left: 1003, top: 572, position: 'absolute', color: '#CBCBCB', fontSize: 40, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>0</div>
+  <div style={{left: 168, top: 340, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Área:</div>
+  <div style={{left: 168, top: 454, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Área:</div>
+  <div style={{left: 168, top: 568, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Área:</div>
+  <div style={{left: 168, top: 682, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Área:</div>
+  <div style={{left: 168, top: 796, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Área:</div>
+  <div style={{left: 168, top: 372, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Horario:</div>
+  <div style={{left: 168, top: 486, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Horario:</div>
+  <div style={{left: 168, top: 600, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Horario:</div>
+  <div style={{left: 168, top: 714, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Horario:</div>
+  <div style={{left: 168, top: 828, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Horario:</div>
+  <div style={{width: 142, height: 52, left: 282, top: 342, position: 'absolute', color: '#CBCBCB', fontSize: 24, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>área... <br/>Horario... </div>
+  <div style={{width: 142, height: 52, left: 282, top: 456, position: 'absolute', color: '#CBCBCB', fontSize: 24, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>área... <br/>Horario... </div>
+  <div style={{width: 142, height: 52, left: 282, top: 572, position: 'absolute', color: '#CBCBCB', fontSize: 24, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>área... <br/>Horario... </div>
+  <div style={{width: 142, height: 52, left: 282, top: 684, position: 'absolute', color: '#CBCBCB', fontSize: 24, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>área... <br/>Horario... </div>
+  <div style={{width: 142, height: 52, left: 280, top: 796, position: 'absolute', color: '#CBCBCB', fontSize: 24, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>área... <br/>Horario... </div>
+  <div style={{left: 123, top: 12, position: 'absolute', color: 'white', fontSize: 48, fontFamily: 'Lato', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Control de horarios</div>
+</div>
+        </div>
+     );
+}
+ 
+export default Prueba;
